c49019b2e2daca48d896d659755f257c377b7c4e
sweauxui <125528967+sweauxui@users.noreply.github.com>
2023-08-01 05:01:17 +0900

    Update index.html

--- W_js/모달.js
+++ W_js/모달.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function(){
     $("#confirm").click(function(){
         modalClose();
@@ -12,19 +11,4 @@ $(function(){
     function modalClose(){
       $("#popup").fadeOut();
     }
-=======
-$(function(){
-    $("#confirm").click(function(){
-        modalClose();
-        //컨펌 이벤트 처리
-    });
-    $("#modal-open").click(function(){        $("#popup").css('display','flex').hide().fadeIn();
-    });
-    $("#close").click(function(){
-        modalClose();
-    });
-    function modalClose(){
-      $("#popup").fadeOut();
-    }
->>>>>>> 4825fc604952e9db9288ce656ffdeaf8a59a8e7a
   });
